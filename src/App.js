6c4add01b018cd890629eabd760460a4d48f172a
Majad in <majad.inayat@sogeti.com>
2020-07-22 21:10:27 +0200

    added css library for form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand">React Form</a>
+      </nav>
+      <div className='container' style={{ width: '400px', marginTop: 20 }}>
+        <form action="">
+          <fieldset>
+            <label htmlFor="text">Add title</label>
+            <input type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Add title">
+            </input>
+          </fieldset>
+        </form>
+
+      </div>
     </div>
   );
 }
