4f873228be115b894f947d5dbdeac80af93ce7a6
Majad in <majad.inayat@sogeti.com>
2020-07-23 09:44:23 +0200

    input for the form

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ function App() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand">React Form</a>
+        <span className="navbar-brand" href="#">React Form</span>
       </nav>
       <div className='container' style={{ width: '400px', marginTop: 20 }}>
         <form action="">
           <fieldset>
-            <label htmlFor="text">Add title</label>
-            <input type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Add title">
-            </input>
+            <div className="form-group">
+              <label htmlFor="text">Add title</label>
+              <input type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Add title"
+              />
+            </div>
           </fieldset>
         </form>
 
