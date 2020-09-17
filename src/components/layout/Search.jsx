5e47056460adeab0869693c40cbac5ef80ac5bb5
Majad In <majadin@Majads-MacBook-Pro.local>
2020-09-17 08:19:50 +0200

    search page

---
+++ src/components/layout/Search.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react"
+const [error, setError] = useState(null)
+const [loading, setLoading] = useState(true)
+const Search = () => {
+  const [data, setData] = useState([])
+  //  const [error, setError] = useState(null)
+  // const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function init() {
+      try {
+        const res = await fetch(URL.API_URL)
+        if (res.ok) {
+          const json = await res.json()
+          setData(json.results)
+        } else {
+          throw res
+        }
+      } catch (e) {
+        setError(e)
+      } finally {
+        setLoading(false)
+      }
+    }
+    init()
+  }, [])
+
+  function handelSubmit(e) {
+    console.log(e.target.value)
+  }
+  return (
+    <div>
+      <input
+        onChange={handelSubmit}
+        className="form-control mr-sm-2"
+        type="text"
+        placeholder="Search"
+      ></input>
+    </div>
+  )
+}
+
+export default Search
