cbe43dea371da91cf48ab87333fe8c5c13e5a6d2
Majad In <majadin@Majads-MacBook-Pro.local>
2020-09-23 12:52:25 +0200

    searh bar is working :tada:

--- src/components/layout/Search.jsx
+++ src/components/layout/Search.jsx
@@ -5,7 +5,7 @@ import {
   baseURL,
   BASE_LANGUAGE_URL_PATH,
 } from "../../services/util/utility";
-
+import MovieCard from "../layout/MovieCard";
 const fetchApi = async (search) => {
   const res = await fetch(
     `${SEARCH_MOVIE_PATH}api_key=${baseURL}${BASE_LANGUAGE_URL_PATH}&query=${search}&page=1&include_adult=false`
@@ -18,43 +18,32 @@ const fetchApi = async (search) => {
 };
 
 const Search = () => {
-  const [search, setSearch] = useState("");
-  const { data } = useQuery(search, fetchApi, {
-    retry: false,
-  });
+  const [search, setSearch] = useState(" ");
+  const { data, isFetching } = useQuery(search, fetchApi);
+
+  // console.log(isFetching);
 
   async function handleChange(e) {
     e.preventDefault();
-    await console.log(setSearch(e.target.value));
+    await setSearch(e.target.value);
   }
-
-  // async function handleSubmit(e) {
-  //   e.preventDefault();
-  //   await setSearch((curSearch) => {
-  //     return {
-  //       ...curSearch,
-  //       [e.target.id]: e.target.value,
-  //     };
-  //   });
-  // }
-
   return (
     <>
-      <div className="p-5 m-4 border border">
+      <div className="p-5 m-4 ">
         <form className=" my-2 my-lg-0">
+          <label htmlFor="search bar">
+            <h1>Seach</h1>
+          </label>
           <input
             className="form-control mr-sm-2"
             type="text"
-            id="search"
+            id="search bar"
             placeholder="Search"
             value={search}
             onChange={handleChange}
           />
-          {/* <button className="btn btn-info" type="submit" value={search}>
-            Search
-          </button> */}
         </form>
-        <div>{JSON.stringify(data, null, 2)}</div>
+        <div>{!isFetching ? <MovieCard props={data} /> : ""}</div>
       </div>
     </>
   );
